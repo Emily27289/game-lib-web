fa2830acfab7481045aed82ab80d6489c0fab8c2
correialeo <leandroo774@outlook.com>
2025-03-26 21:04:21 -0300

    fix: passando prop com o nome correto

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -3,7 +3,7 @@ import NavBar from "@/app/components/nav-bar";
 export default function GamesPage() {
   return (
     <div>
-      <NavBar active= "Games" />
+      <NavBar active= "Game" />
       <div className="p-6">
         <h1 className="text-2xl font-bold">Lista de Jogos</h1>
         <p>Jogos</p>
